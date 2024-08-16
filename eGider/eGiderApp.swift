ae4b71252d840ad3541ead7ca91563087634354c
turalbabayev <turalbabayev@outlook.com.tr>
2024-08-16 18:18:29 +0300

    Initial Commit

---
+++ eGider/eGiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  eGiderApp.swift
+//  eGider
+//
+//  Created by Tural Babayev on 16.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct eGiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
